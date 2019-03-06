3a1b1446f0a1847c2cb6d897579acec39f15763b
Dmytro Chukmasov <d.chukmasov@gmail.com>
2019-03-06 11:48:10 +0200

    added universal slice is not empty; removed substituted
    Signed-off-by: Dmytro Chukmasov <d.chukmasov@gmail.com>

---
+++ validators/slice_is_not_empty_test.go
@@ -0,0 +1,57 @@
+package validators
+
+import (
+	"testing"
+
+	"github.com/s3rj1k/validator"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_SliceIsNotEmpty(t *testing.T) {
+
+	r := require.New(t)
+
+	var tests = []struct {
+		field interface{}
+		valid bool
+	}{
+		{[]int{10, 11, 55, -10}, true},
+		{[]uintptr{1, 11, 1111, 0}, true},
+		{[]string{"asd", "qwe", "qwe"}, true},
+		{[]float32{0, 15.5}, true},
+		{[]string{""}, true},
+		{[]byte("here is the byte slice"), true},
+		{[]rune{'r', 'u', 'n', 'e'}, true},
+		{[]rune("runes"), true},
+
+		{[]rune(""), false},
+		{[]byte(""), false},
+		{[]int{}, false},
+		{[]uintptr{}, false},
+		{[]string{}, false},
+		{[]byte{}, false},
+	}
+
+	for _, test := range tests {
+		v := &SliceIsNotEmpty{Name: "Slice", Field: test.field}
+		e := validator.NewErrors()
+		v.Validate(e)
+
+		r.Equal(!test.valid, e.HasAny())
+		if !test.valid {
+			r.Equal([]string{SliceIsNotEmptyError(v)}, e.Get(v.Name))
+		}
+	}
+
+	v := &SliceIsUnique{Name: "Slice", Field: nil}
+	e := validator.NewErrors()
+	v.Validate(e)
+	r.Equal(1, e.Count())
+	r.Equal([]string{ErrNilValue.Error()}, e.Get(v.Name))
+
+	v = &SliceIsUnique{Name: "Slice", Field: []struct{}{}}
+	e = validator.NewErrors()
+	v.Validate(e)
+	r.Equal([]string{ErrBadSliceType.Error()}, e.Get(v.Name))
+}
