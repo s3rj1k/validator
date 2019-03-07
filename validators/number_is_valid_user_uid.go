772b54eb90f4aa5069d6adebffe3a378776ee085
s3rj1k <evasive.gyron@gmail.com>
2019-03-07 13:09:52 +0200

    rename UID validator to user UID
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

--- validators/number_is_valid_user_uid.go
+++ validators/number_is_valid_user_uid.go
@@ -13,27 +13,27 @@ import (
 
 // nolint: gochecknoglobals
 var (
-	// DefaultMinUID is a default value for MinUID, used then parsing of 'login.defs' fails.
-	DefaultMinUID uint64 = 1000
+	// DefaultMinUserUID is a default value for MinUserUID, used then parsing of 'login.defs' fails.
+	DefaultMinUserUID uint64 = 1000
 
-	// DefaultMaxUID is a default value for MaxUID, used then parsing of 'login.defs' fails.
-	DefaultMaxUID uint64 = 60000
+	// DefaultMaxUserUID is a default value for MaxUserUID, used then parsing of 'login.defs' fails.
+	DefaultMaxUserUID uint64 = 60000
 )
 
-// NumberIsValidUIDError is a function that defines error message returned by NumberIsValidUID validator.
+// NumberIsValidUserUIDError is a function that defines error message returned by NumberIsValidUserUID validator.
 // nolint: gochecknoglobals
-var NumberIsValidUIDError = func(v *NumberIsValidUID) string {
-	return fmt.Sprintf("%d is not valid UID", v.Field)
+var NumberIsValidUserUIDError = func(v *NumberIsValidUserUID) string {
+	return fmt.Sprintf("%d is not valid user UID", v.Field)
 }
 
-// NumberIsValidUID is a validator object.
-type NumberIsValidUID struct {
+// NumberIsValidUserUID is a validator object.
+type NumberIsValidUserUID struct {
 	Name  string
 	Field interface{}
 }
 
 // Validate adds an error if the Field is in range of UID_MIN, UID_MAX from '/etc/login.defs'.
-func (v *NumberIsValidUID) Validate(e *validator.Errors) {
+func (v *NumberIsValidUserUID) Validate(e *validator.Errors) {
 
 	fNum, err := cast(v.Field)
 	if err != nil {
@@ -42,49 +42,49 @@ func (v *NumberIsValidUID) Validate(e *validator.Errors) {
 		return
 	}
 
-	minUID, maxUID := readUIDRange(LoginDefsPath)
+	minUserUID, maxUserUID := readUserUIDRange(LoginDefsPath)
 
 	//  for os.Chown func a uid or gid of -1 means to not change that value
 	if fNum.isNegative && fNum.Value == 1 {
 		return
 	}
 
-	if fNum.Value >= minUID &&
-		fNum.Value <= maxUID &&
+	if fNum.Value >= minUserUID &&
+		fNum.Value <= maxUserUID &&
 		!fNum.isNegative {
 
 		return
 	}
 
-	e.Add(v.Name, NumberIsValidUIDError(v))
+	e.Add(v.Name, NumberIsValidUserUIDError(v))
 }
 
 // SetField sets validator field.
-func (v *NumberIsValidUID) SetField(s interface{}) {
+func (v *NumberIsValidUserUID) SetField(s interface{}) {
 	v.Field = s
 }
 
 // SetNameIndex sets index of slice element on Name.
-func (v *NumberIsValidUID) SetNameIndex(i int) {
+func (v *NumberIsValidUserUID) SetNameIndex(i int) {
 	v.Name = fmt.Sprintf("%s[%d]", regexp.MustCompile(`\[[0-9]+\]$`).ReplaceAllString(v.Name, ""), i)
 }
 
 // GetName is a getter on Name field.
-func (v *NumberIsValidUID) GetName() string {
+func (v *NumberIsValidUserUID) GetName() string {
 	return v.Name
 }
 
-// readUIDRange parses 'login.defs' file.
-func readUIDRange(path string) (uint64, uint64) {
+// readUserUIDRange parses 'login.defs' file.
+func readUserUIDRange(path string) (uint64, uint64) {
 
 	var (
-		minUID = DefaultMinUID
-		maxUID = DefaultMaxUID
+		minUserUID = DefaultMinUserUID
+		maxUserUID = DefaultMaxUserUID
 	)
 
 	fd, err := os.Open(path)
 	if err != nil {
-		return minUID, maxUID
+		return minUserUID, maxUserUID
 	}
 
 	defer func(fd *os.File) {
@@ -102,17 +102,17 @@ func readUIDRange(path string) (uint64, uint64) {
 
 		if fields[0] == "UID_MIN" {
 			if i, err := strconv.ParseUint(fields[1], 10, 64); err == nil {
-				minUID = i
+				minUserUID = i
 			}
 		}
 
 		if fields[0] == "UID_MAX" {
 			if i, err := strconv.ParseUint(fields[1], 10, 64); err == nil {
-				maxUID = i
+				maxUserUID = i
 			}
 		}
 
 	}
 
-	return minUID, maxUID
+	return minUserUID, maxUserUID
 }
