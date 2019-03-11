7b8d25453c4538821f6a790ebd039bdf5da946a0
Dmytro Chukmasov <d.chukmasov@gmail.com>
2019-03-11 16:46:51 +0200

    general IP address validators
    Signed-off-by: Dmytro Chukmasov <d.chukmasov@gmail.com>

---
+++ validators/string_is_ipv4.go
@@ -0,0 +1,57 @@
+package validators
+
+import (
+	"fmt"
+	"net"
+	"regexp"
+	"strings"
+
+	"github.com/s3rj1k/validator"
+)
+
+// StringIsIPv4Error is a function that defines error message returned by StringIsIPv4 validator.
+// nolint: gochecknoglobals
+var StringIsIPv4Error = func(v *StringIsIPv4) string {
+	return fmt.Sprintf("'%s' must be IPv4", v.Name)
+}
+
+// StringIsIPv4 is a validator object.
+type StringIsIPv4 struct {
+	Name  string
+	Field string
+}
+
+// Validate adds an error if the Field is not a valid IPv4 address.
+func (v *StringIsIPv4) Validate(e *validator.Errors) {
+
+	if isIPv4(v.Field) {
+		return
+	}
+
+	e.Add(v.Name, StringIsIPv4Error(v))
+}
+
+// SetField sets validator field.
+func (v *StringIsIPv4) SetField(s string) {
+	v.Field = s
+}
+
+// SetNameIndex sets index of slice element on Name.
+func (v *StringIsIPv4) SetNameIndex(i int) {
+	v.Name = fmt.Sprintf("%s[%d]", regexp.MustCompile(`\[[0-9]+\]$`).ReplaceAllString(v.Name, ""), i)
+}
+
+// checks if s is a valid IPv4
+func isIPv4(s string) bool {
+
+	if len(s) == 0 {
+		return false
+	}
+
+	ip := net.ParseIP(s)
+	if ip.To4() == nil || !strings.Contains(s, ".") {
+		return false
+	}
+
+	return true
+}
