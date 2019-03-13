fe20aec5f8e6ec2716ec9aa1091ae4b0fbe7d2d0
Dmytro Chukmasov <d.chukmasov@gmail.com>
2019-03-13 12:13:41 +0200

    validators of executable files added
    Signed-off-by: Dmytro Chukmasov <d.chukmasov@gmail.com>

---
+++ validators/string_file_is_executable.go
@@ -0,0 +1,50 @@
+package validators
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/s3rj1k/validator"
+)
+
+// StringFileIsExecutableError is a function that defines error message returned by StringFileIsExecutable validator.
+// nolint: gochecknoglobals
+var StringFileIsExecutableError = func(v *StringFileIsExecutable) string {
+	return fmt.Sprintf("'%s' does not have execute bit set", v.Name)
+}
+
+// StringFileIsExecutable is a validator object.
+type StringFileIsExecutable struct {
+	Name  string
+	Field string
+}
+
+// Validate adds an error if the Field is a file without execute bit set.
+func (v *StringFileIsExecutable) Validate(e *validator.Errors) {
+
+	fi, err := os.Stat(v.Field)
+	if err != nil {
+		return
+	}
+
+	if fi.IsDir() {
+		return
+	}
+
+	if fi.Mode()&0111 == 0111 {
+		return
+	}
+
+	e.Add(v.Name, StringFileIsExecutableError(v))
+}
+
+// SetField sets validator field.
+func (v *StringFileIsExecutable) SetField(s string) {
+	v.Field = s
+}
+
+// SetNameIndex sets index of slice element on Name.
+func (v *StringFileIsExecutable) SetNameIndex(i int) {
+	v.Name = fmt.Sprintf("%s[%d]", regexp.MustCompile(`\[[0-9]+\]$`).ReplaceAllString(v.Name, ""), i)
+}
---
+++ validators/string_file_is_not_executable.go
@@ -0,0 +1,46 @@
+package validators
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/s3rj1k/validator"
+)
+
+// StringFileIsNotExecutableError is a function that defines error message returned by StringFileIsNotExecutable validator.
+// nolint: gochecknoglobals
+var StringFileIsNotExecutableError = func(v *StringFileIsNotExecutable) string {
+	return fmt.Sprintf("'%s' has execute bit set", v.Name)
+}
+
+// StringFileIsNotExecutable is a validator object.
+type StringFileIsNotExecutable struct {
+	Name  string
+	Field string
+}
+
+// Validate adds an error if the Field is a file with execute bit set.
+func (v *StringFileIsNotExecutable) Validate(e *validator.Errors) {
+
+	fi, err := os.Stat(v.Field)
+	if err != nil {
+		return
+	}
+
+	if fi.IsDir() || fi.Mode()&0111 != 0111 {
+		return
+	}
+
+	e.Add(v.Name, StringFileIsNotExecutableError(v))
+}
+
+// SetField sets validator field.
+func (v *StringFileIsNotExecutable) SetField(s string) {
+	v.Field = s
+}
+
+// SetNameIndex sets index of slice element on Name.
+func (v *StringFileIsNotExecutable) SetNameIndex(i int) {
+	v.Name = fmt.Sprintf("%s[%d]", regexp.MustCompile(`\[[0-9]+\]$`).ReplaceAllString(v.Name, ""), i)
+}
