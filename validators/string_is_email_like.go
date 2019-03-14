018adfd377b20bf30b6102e94b5b17c981e5fe1c
Dmytro Chukmasov <d.chukmasov@gmail.com>
2019-03-14 17:49:43 +0200

    string_is_email and email_like validators splitted
    Signed-off-by: Dmytro Chukmasov <d.chukmasov@gmail.com>

---
+++ validators/string_is_email_like.go
@@ -0,0 +1,59 @@
+package validators
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/s3rj1k/validator"
+)
+
+// StringIsEmailLikeError is a function that defines error message returned by StringIsEmailLike validator.
+// nolint: gochecknoglobals
+var StringIsEmailLikeError = func(v *StringIsEmailLike) string {
+	return fmt.Sprintf("%s does not match the email format", v.Name)
+}
+
+// StringIsEmailLike is a validator object.
+type StringIsEmailLike struct {
+	Name  string
+	Field string
+}
+
+// Validate adds an error if the Field does not correspond to "username@domain" structure.
+// It also checks that domain has a domain zone (but does not check if the zone is valid).
+// Also allows inner and outer whitespaces.
+func (v *StringIsEmailLike) Validate(e *validator.Errors) {
+
+	var validStructure = false
+	var domainZonePresent = false
+
+	parts := strings.Split(v.Field, "@")
+
+	if len(parts) == 2 && len(parts[0]) > 0 && len(parts[1]) > 0 {
+		validStructure = true
+	}
+
+	if len(parts) == 2 {
+		domain := parts[1]
+		// Check that domain is valid
+		if len(strings.Split(domain, ".")) >= 2 {
+			domainZonePresent = true
+		}
+	}
+
+	if !validStructure || !domainZonePresent {
+		e.Add(v.Name, StringIsEmailLikeError(v))
+		return
+	}
+}
+
+// SetField sets validator field.
+func (v *StringIsEmailLike) SetField(s string) {
+	v.Field = s
+}
+
+// SetNameIndex sets index of slice element on Name.
+func (v *StringIsEmailLike) SetNameIndex(i int) {
+	v.Name = fmt.Sprintf("%s[%d]", regexp.MustCompile(`\[[0-9]+\]$`).ReplaceAllString(v.Name, ""), i)
+}
--- validators/string_is_email_like_test.go
+++ validators/string_is_email_like_test.go
@@ -4,11 +4,10 @@ import (
 	"testing"
 
 	"github.com/s3rj1k/validator"
-
 	"github.com/stretchr/testify/require"
 )
 
-func Test_StringIsEmail(t *testing.T) {
+func Test_StringIsEmailLike(t *testing.T) {
 
 	r := require.New(t)
 
@@ -25,6 +24,7 @@ func Test_StringIsEmail(t *testing.T) {
 		{"foo@bar.中文网", true},
 		{"invalidemail@", false},
 		{"invalid.com", false},
+		{"@", false},
 		{"@invalid.com", false},
 		{"test|123@m端ller.com", true},
 		{"hans@m端ller.com", true},
@@ -34,18 +34,24 @@ func Test_StringIsEmail(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		v := &StringIsEmail{Name: "email", Field: test.email}
+		v := &StringIsEmailLike{Name: "email", Field: test.email}
 		e := validator.NewErrors()
 		v.Validate(e)
-		r.Equal(test.valid, !e.HasAny())
+		r.Equal(test.valid, !e.HasAny(), test.email)
 		if !test.valid {
-			r.Equal([]string{StringIsEmailError(v)}, e.Get("email"))
+			r.Equal(e.Get("email"), []string{StringIsEmailLikeError(v)})
 		}
 	}
 
-	v := &StringIsEmail{Name: "email", Field: ""}
+	v := &StringIsEmailLike{Name: "email", Field: "foo@bar"}
 	e := validator.NewErrors()
 	v.Validate(e)
 	r.Equal(e.Count(), 1)
-	r.Equal([]string{StringIsEmailError(v)}, e.Get("email"))
+	r.Equal(e.Get("email"), []string{StringIsEmailLikeError(v)})
+
+	v = &StringIsEmailLike{Name: "email", Field: ""}
+	e = validator.NewErrors()
+	v.Validate(e)
+	r.Equal(e.Count(), 1)
+	r.Equal(e.Get("email"), []string{StringIsEmailLikeError(v)})
 }
