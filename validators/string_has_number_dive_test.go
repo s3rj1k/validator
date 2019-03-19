2b0f0cefb9f097c63451561c7751d6b1351f97ef
Dmytro Chukmasov <d.chukmasov@gmail.com>
2019-03-19 12:30:53 +0200

    added missing dive tests
    Signed-off-by: Dmytro Chukmasov <d.chukmasov@gmail.com>

---
+++ validators/string_has_number_dive_test.go
@@ -0,0 +1,48 @@
+package validators
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/s3rj1k/validator"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_StringHasNumberDive(t *testing.T) {
+
+	r := require.New(t)
+
+	var tests = []struct {
+		field          []string
+		valid          bool
+		invalidIndexes []int
+	}{
+		{[]string{"3085u", "    a", "z135^^", "()#U	a	]]", "124", "#$%% ", "冊;復製品"}, false, []int{1, 3, 5, 6}},
+		{[]string{" ", ""}, false, []int{0, 1}},
+		{nil, false, []int{0}},
+	}
+
+	for index, test := range tests {
+		v := &StringSliceDive{
+			Validator: &StringHasNumber{Name: "HasNumber"},
+			Field:     test.field,
+		}
+		e := validator.NewErrors()
+		v.Validate(e)
+
+		r.Equalf(!test.valid, e.HasAny(), "tc %d", index)
+		if !test.valid {
+
+			r.Equalf(len(test.invalidIndexes), e.Count(), "tc %d wrong number of errors", index)
+
+			errnames := []string{}
+			for _, i := range test.invalidIndexes {
+				errnames = append(errnames, fmt.Sprintf("HasNumber[%d]", i))
+			}
+
+			for _, en := range errnames {
+				r.Containsf(e.JSON(), en, "tc %d", index)
+			}
+		}
+	}
+}
---
+++ validators/string_is_not_numeric_dive_test.go
@@ -0,0 +1,48 @@
+package validators
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/s3rj1k/validator"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_StringIsNotNumericDive(t *testing.T) {
+
+	r := require.New(t)
+
+	var tests = []struct {
+		field          []string
+		valid          bool
+		invalidIndexes []int
+	}{
+		{[]string{"3085u", "    a", "z135^^", "()#U	a	]]", "124", "#$%% ", "冊;復製品", "0"}, false, []int{4, 7}},
+		{[]string{" ", ""}, true, []int{}},
+		{nil, true, []int{}},
+	}
+
+	for index, test := range tests {
+		v := &StringSliceDive{
+			Validator: &StringIsNotNumeric{Name: "IsNotNumeric"},
+			Field:     test.field,
+		}
+		e := validator.NewErrors()
+		v.Validate(e)
+
+		r.Equalf(!test.valid, e.HasAny(), "tc %d", index)
+		if !test.valid {
+
+			r.Equalf(len(test.invalidIndexes), e.Count(), "tc %d wrong number of errors", index)
+
+			errnames := []string{}
+			for _, i := range test.invalidIndexes {
+				errnames = append(errnames, fmt.Sprintf("IsNotNumeric[%d]", i))
+			}
+
+			for _, en := range errnames {
+				r.Containsf(e.JSON(), en, "tc %d", index)
+			}
+		}
+	}
+}
