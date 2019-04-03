9a0002f3983b3c726127fd6d79b62361ab8d5c47
s3rj1k <evasive.gyron@gmail.com>
2019-04-04 00:25:26 +0300

    add string_is_path_and_is_writable validator
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

---
+++ validators/string_is_path_and_is_writable_dive_test.go
@@ -0,0 +1,78 @@
+package validators
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/s3rj1k/validator"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_StringIsPathAndIsWritableDive(t *testing.T) {
+
+	r := require.New(t)
+
+	_ = os.Remove("/tmp/string_writable_file")
+
+	fd, err := os.Create("/tmp/string_writable_file")
+	r.Nil(err)
+
+	err = fd.Chmod(0777)
+	r.Nil(err)
+
+	err = fd.Close()
+	r.Nil(err)
+
+	_ = os.Remove("/tmp/string_not_writable_file")
+
+	fd, err = os.Create("/tmp/string_not_writable_file")
+	r.Nil(err)
+
+	err = fd.Chmod(0000)
+	r.Nil(err)
+
+	err = fd.Close()
+	r.Nil(err)
+
+	var tests = []struct {
+		field          []string
+		invalidIndexes []int
+	}{
+		{[]string{"/tmp/not_exists_i_hope", "/tmp/string_not_writable_file", "/tmp/string_writable_file"}, []int{0, 1}},
+	}
+
+	for index, test := range tests {
+		v := &StringSliceDive{
+			Validator: &StringIsPathAndIsWritable{Name: "StringIsPathAndIsWritableDive"},
+			Field:     test.field,
+		}
+		e := validator.NewErrors()
+		v.Validate(e)
+
+		r.Equalf(len(test.invalidIndexes), e.Count(), "tc %d wrong number of errors", index)
+
+		errnames := []string{}
+		for _, i := range test.invalidIndexes {
+			errnames = append(errnames, fmt.Sprintf("StringIsPathAndIsWritableDive[%d]", i))
+		}
+
+		for _, en := range errnames {
+			r.Containsf(e.JSON(), en, "tc %d", index)
+		}
+
+	}
+
+	err = os.Chmod("/tmp/string_not_writable_file", 0777)
+	r.Nil(err)
+
+	err = os.Remove("/tmp/string_not_writable_file")
+	r.Nil(err)
+
+	err = os.Chmod("/tmp/string_writable_file", 0777)
+	r.Nil(err)
+
+	err = os.Remove("/tmp/string_writable_file")
+	r.Nil(err)
+}
---
+++ validators/string_is_path_and_is_writable_test.go
@@ -0,0 +1,69 @@
+package validators
+
+import (
+	"os"
+	"testing"
+
+	"github.com/s3rj1k/validator"
+
+	"github.com/stretchr/testify/require"
+)
+
+// nolint: gosec
+func Test_StringIsPathAndIsWritable(t *testing.T) {
+
+	r := require.New(t)
+
+	_ = os.Remove("/tmp/string_writable_file")
+
+	fd, err := os.Create("/tmp/string_writable_file")
+	r.Nil(err)
+
+	v := &StringIsPathAndIsWritable{Name: "Name", Field: "/tmp/string_writable_file"}
+	e := validator.NewErrors()
+	v.Validate(e)
+	r.Equal(0, e.Count())
+
+	err = fd.Chmod(0000)
+	r.Nil(err)
+
+	v = &StringIsPathAndIsWritable{Name: "Name", Field: "/tmp/string_writable_file"}
+	e = validator.NewErrors()
+	v.Validate(e)
+	r.Equal(1, e.Count())
+	r.Equal([]string{StringIsPathAndIsWritableError(v)}, e.Get("Name"))
+
+	err = fd.Chmod(0777)
+	r.Nil(err)
+
+	err = fd.Close()
+	r.Nil(err)
+
+	err = os.Remove("/tmp/string_writable_file")
+	r.Nil(err)
+
+	_ = os.Remove("/tmp/string_writable_dir")
+
+	err = os.MkdirAll("/tmp/string_writable_dir", 0777)
+	r.Nil(err)
+
+	v = &StringIsPathAndIsWritable{Name: "Name", Field: "/tmp/string_writable_dir"}
+	e = validator.NewErrors()
+	v.Validate(e)
+	r.Equal(0, e.Count())
+
+	err = os.Chmod("/tmp/string_writable_dir", 0000)
+	r.Nil(err)
+
+	v = &StringIsPathAndIsWritable{Name: "Name", Field: "/tmp/string_writable_dir"}
+	e = validator.NewErrors()
+	v.Validate(e)
+	r.Equal(1, e.Count())
+	r.Equal([]string{StringIsPathAndIsWritableError(v)}, e.Get("Name"))
+
+	err = os.Chmod("/tmp/string_writable_dir", 0777)
+	r.Nil(err)
+
+	err = os.Remove("/tmp/string_writable_dir")
+	r.Nil(err)
+}
